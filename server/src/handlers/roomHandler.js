1f62689f4efabe99cccf73d1e73182ca7091ff66
Aayush895 <aayush895@gmail.com>
2026-02-07 23:51:41 +0530

    Implemented user joining a particular room - 1

--- server/src/handlers/roomHandler.js
+++ server/src/handlers/roomHandler.js
@@ -1,15 +1,33 @@
 import { v4 as UUIDv4 } from 'uuid'
 
 export function roomHandler(socket) {
+  const rooms = {}
   const roomId = UUIDv4()
 
   function createRoom() {
     socket.join(roomId)
     console.log('Room is created with id: ', roomId)
+    rooms[roomId] = []
     socket.emit('room-created', { roomId })
   }
 
-  function joinRoom() {}
+  function joinRoom({ roomId, peerId }) {
+    if (rooms[roomId]) {
+      rooms[roomId].push(peerId)
+      socket.join(roomId)
+
+      socket.on('ready', () => {
+        socket.to(roomId).emit('user-joined', { peerId })
+      })
+
+      console.log('Users in rooms: ', rooms)
+
+      socket.emit('get-users', {
+        roomId,
+        participants: rooms[roomId],
+      })
+    }
+  }
 
   socket.on('create-room', createRoom)
   socket.on('join-room', joinRoom)
